374e494002fe7d8abbf60d5e69b58c493f5ae679
Jarrod Dowalter <35478547+jarroddowalter@users.noreply.github.com>
2021-03-05 14:42:45 -0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+		"react/react-in-jsx-scope": "off",
+		"react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+		"react/display-name": 1
+    }
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/main.scss' // Import Global scss/css here
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
