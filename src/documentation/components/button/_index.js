4d141df0ac6d05714a3d911caa00470905262561
yeonygg <vivienne@gumgum.com>
2021-04-02 16:59:58 -0400

    added pill buttons

--- src/documentation/components/button/_index.js
+++ src/documentation/components/button/_index.js
@@ -1,6 +1,8 @@
 import ReactDOMServer from "react-dom/server";
 import base from "./base";
 import sizes from "./sizes";
+import disabled from "./disabled";
+import pill from "./pill";
 
 export default {
 	title: "Button",
@@ -17,6 +19,18 @@ export default {
 			name: "Sizes",
 			description: "",
 			markup: ReactDOMServer.renderToStaticMarkup(sizes),
+			dark: false,
+		},
+		{
+			name: "Disabled",
+			description: "",
+			markup: ReactDOMServer.renderToStaticMarkup(disabled),
+			dark: false,
+		},
+		{
+			name: "Pill",
+			description: "",
+			markup: ReactDOMServer.renderToStaticMarkup(pill),
 			dark: false,
 		},
 
---
+++ src/documentation/components/button/pill.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+
+export default (
+<Fragment>	
+
+
+
+<button className="pier-button-pill--sm">Button Text</button>
+<button className="pier-button-pill--md">Button Text</button>
+<button className="pier-button-pill--lg">Button Text</button>
+
+
+
+</Fragment>
+
+
+	
+);
