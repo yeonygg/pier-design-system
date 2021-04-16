3133fd9ec82b81403948ea5d296243515bbfb75e
yeonygg <vivienne@gumgum.com>
2021-04-16 18:55:38 -0400

    buttons and inputs updated

--- src/documentation/components/button-standard/base.js
+++ src/documentation/components/button-standard/base.js
@@ -5,3 +5,4 @@ export default (
         <button className="pier-button-standard pier-button-standard--md">Button Text</button>
     </Fragment>
 );
+// rename to pier-button-standard__icon!!
--- src/documentation/components/input-text/errors.js
+++ src/documentation/components/input-text/errors.js
@@ -1,9 +1,21 @@
 import { Fragment } from 'react';
 
 export default (
+<<<<<<< HEAD
     <Fragment>
         <div className="pier-input-container--error">
             <input className="pier-input-text--errors" type="text" placeholder="Error" />
         </div>
     </Fragment>
+=======
+	<Fragment>
+<<<<<<< HEAD
+		<div className='pier-input-container--error'>
+			<input className='pier-input-text--errors' type='text' placeholder='Error' />
+		</div>
+=======
+	<input className="pier-input-text pier-input-text--md pier-input-text--error" type="text" placeholder="Error" />
+>>>>>>> e3ecc85... buttons and inputs updated
+	</Fragment>
+>>>>>>> 1a00f86... buttons and inputs updated
 );
