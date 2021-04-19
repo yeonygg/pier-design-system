fc1b94b7b09c3d60321a5b76a71425f82950d6ee
yeonygg <vivienne@gumgum.com>
2021-04-19 19:28:08 -0400

    inputs updated 4/19

--- src/documentation/components/input-text/_index.js
+++ src/documentation/components/input-text/_index.js
@@ -1,77 +1,74 @@
-import ReactDOMServer from "react-dom/server";
-import base from "./base";
-import sizes from "./sizes";
-import error from "./error";
-import pill from "./pill";
-import disabled from "./disabled";
-import capleft from "./cap-left";
-import capright from "./cap-right";
-import dark from "./dark";
-import errorDark from "./error-dark";
-
+import ReactDOMServer from 'react-dom/server';
+import base from './base';
+import sizes from './sizes';
+import error from './error';
+import pill from './pill';
+import disabled from './disabled';
+import capleft from './cap-left';
+import capright from './cap-right';
+import dark from './dark';
+import errorDark from './error-dark';
 
 export default {
-	title: "Input - Text",
-	slug: "input-text",
-	description: "This is a description for input-text.",
-	"last-updated": "4/6/21",
-	variations: [
-		{
-			name: "Base",
-			description: "",
-			markup: ReactDOMServer.renderToStaticMarkup(base),
-			dark: false,
-		},
-		{
-			name: "Sizes",
-			description: "",
-			markup: ReactDOMServer.renderToStaticMarkup(sizes),
-			dark: false,
-		},
-		{
-			name: "Error",
-			description: "",
-			markup: ReactDOMServer.renderToStaticMarkup(error),
-			dark: false,
-		},
-		{
-			name: "Pill",
-			description: "",
-			markup: ReactDOMServer.renderToStaticMarkup(pill),
-			dark: false,
-		},
-		{
-			name: "Disabled",
-			description: "",
-			markup: ReactDOMServer.renderToStaticMarkup(disabled),
-			dark: false,
-		},
-<<<<<<< HEAD
-		{
-			name: "Cap-left",
-			description: "",
-			markup: ReactDOMServer.renderToStaticMarkup(capleft),
-			dark: false,
-		},
-		{
-			name: "Cap-right",
-			description: "",
-			markup: ReactDOMServer.renderToStaticMarkup(capright),
-			dark: false,
-		},
-		{
-			name: "Dark",
-			description: "",
-			markup: ReactDOMServer.renderToStaticMarkup(dark),
-			dark: true,
-		},
-		{
-			name: "Error Dark",
-			description: "",
-			markup: ReactDOMServer.renderToStaticMarkup(errorDark),
-			dark: true,
-		}
-=======
->>>>>>> e3ecc85... buttons and inputs updated
-	],
+    title: 'Input - Text',
+    slug: 'input-text',
+    description: 'This is a description for input-text.',
+    'last-updated': '4/6/21',
+    variations: [
+        {
+            name: 'Base',
+            description: '',
+            markup: ReactDOMServer.renderToStaticMarkup(base),
+            dark: false,
+        },
+        {
+            name: 'Sizes',
+            description: '',
+            markup: ReactDOMServer.renderToStaticMarkup(sizes),
+            dark: false,
+        },
+        {
+            name: 'Error',
+            description: '',
+            markup: ReactDOMServer.renderToStaticMarkup(error),
+            dark: false,
+        },
+        {
+            name: 'Pill',
+            description: '',
+            markup: ReactDOMServer.renderToStaticMarkup(pill),
+            dark: false,
+        },
+        {
+            name: 'Disabled',
+            description: '',
+            markup: ReactDOMServer.renderToStaticMarkup(disabled),
+            dark: false,
+        },
+
+        {
+            name: 'Cap-left',
+            description: '',
+            markup: ReactDOMServer.renderToStaticMarkup(capleft),
+            dark: false,
+        },
+        {
+            name: 'Cap-right',
+            description: '',
+            markup: ReactDOMServer.renderToStaticMarkup(capright),
+            dark: false,
+        },
+        {
+            name: 'Dark',
+            description: '',
+            markup: ReactDOMServer.renderToStaticMarkup(dark),
+            dark: true,
+        },
+        {
+            name: 'Error Dark',
+            description: '',
+            markup: ReactDOMServer.renderToStaticMarkup(errorDark),
+            dark: true,
+        },
+    ],
 };
--- src/documentation/components/input-text/error.js
+++ src/documentation/components/input-text/error.js
@@ -1,9 +1,7 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 
 export default (
 	<Fragment>
-		
-			<input className='pier-input-text pier-input-text--md pier-input-text--error' type='text' placeholder='Error' />
-
+	<input className="pier-input-text pier-input-text--md pier-input-text--error" type="text" placeholder="Error" />
 	</Fragment>
 );
