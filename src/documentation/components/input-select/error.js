2dff35dcfdbc0de03cd2150d3ee836c96b56517f
yeonygg <vivienne@gumgum.com>
2021-04-21 12:39:39 -0400

    input select updated

--- src/documentation/components/input-select/error.js
+++ src/documentation/components/input-select/error.js
@@ -2,6 +2,16 @@ import { Fragment } from "react";
 
 export default (
 	<Fragment>
-	<input className="pier-input-text pier-input-text--md pier-input-text--error" type="text" placeholder="Error" />
+
+	<select class="pier-input-select pier-input-select--md pier-input-select--error" required>
+	<option value="" disabled="" selected="" hidden="">Select your option</option>
+	<option value="1">Option 1</option>
+	<option value="2">Option 2</option>
+	<option value="3">Option 3</option>
+	<option value="4">Option 4</option>
+	<option value="5">Option 5</option>
+	<option value="6">Option 6</option>
+	</select>
+
 	</Fragment>
 );
