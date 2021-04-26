cfb9cd0053016bc9a736911231f0ef0e54447c5a
yeonygg <vivienne@gumgum.com>
2021-04-26 16:51:15 -0400

    updated checkbox

--- src/documentation/components/input-checkbox/base.js
+++ src/documentation/components/input-checkbox/base.js
@@ -1,15 +1,29 @@
 import { Fragment } from "react";
 
 export default (
-	<Fragment>
-	<div className="pier-input-checkbox pier-input-checkbox--md">
-    <input className="pier-input-checkbox__input" type="checkbox" name="example-1" value="" />
-	
-    <div className="pier-input-checkbox__indicator"></div>
-    <label className="pier-input-checkbox__label" for="check">Label</label>
-	</div>
+  <Fragment>
+    <label className="pier-input-checkbox pier-input-checkbox--md">
+      <input
+        className="pier-input-checkbox__input"
+        type="checkbox"
+        name="example-1"
+        value=""
+      />
 
-	</Fragment>
+      <div className="pier-input-checkbox__indicator"></div>
+      <span className="pier-input-checkbox__label">Label</span>
+    </label>
 
+    <label className="pier-input-checkbox pier-input-checkbox--md">
+      <input
+        className="pier-input-checkbox__input"
+        type="checkbox"
+        name="example-1"
+        value=""
+      />
 
+      <div className="pier-input-checkbox__indicator"></div>
+      <span className="pier-input-checkbox__label">Label</span>
+    </label>
+  </Fragment>
 );
