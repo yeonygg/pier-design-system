9bea43be1a622f1489270812f454329c521bc454
yeonygg <vivienne@gumgum.com>
2021-04-29 12:44:37 -0400

    added toggle css branch

---
+++ src/documentation/components/input-toggle/base.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+
+export default (
+    <Fragment>
+        <label className="pier-input-checkbox pier-input-checkbox--md">
+            <input className="pier-input-checkbox__input" type="checkbox" name="example-1" value="" />
+
+            <div className="pier-input-checkbox__indicator"></div>
+            <span className="pier-input-checkbox__label">Label</span>
+        </label>
+    </Fragment>
+);
