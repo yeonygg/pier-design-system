132e8e2ab5fc67c1b9e463a08fce08699ed101b1
yeonygg <vivienne@gumgum.com>
2021-05-04 13:17:43 -0400

    toggle and group input added

--- src/documentation/components/input-group/input-combo-group.js
+++ src/documentation/components/input-group/input-combo-group.js
@@ -4,7 +4,7 @@ export default (
     <Fragment>
    
     <h4 className="pier-label pier-label--sm -m-b-4">Text Input Label</h4>
-    <div class="pier-input-combo">
+    <div className="pier-input-combo">
     <input className="pier-input-text pier-input-text--cap-left" type="text" placeholder="Enter Text" />
     <button className="pier-button-hero pier-button-hero--cap-right">
     <span className="pier-button-hero__content">
