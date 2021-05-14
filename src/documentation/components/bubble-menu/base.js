8caf97c8a2a27511cfa9ea5689daa9fb8d6df69c
yeonygg <vivienne@gumgum.com>
2021-05-14 13:52:30 -0400

    updated bubble menu

--- src/documentation/components/bubble-menu/base.js
+++ src/documentation/components/bubble-menu/base.js
@@ -6,11 +6,13 @@ export default (
         <button className="pier-button-icon">
             <i className="far fa-bell pier-button-icon__icon"></i>
         </button>
-        <div className="pier-bubble-menu pier-bubble--active">
+        <div className="pier-bubble-menu">
         <ul className="pier-bubble-menu--list">
         <li><div class="pier-bubble-menu--list-link">Alert 1</div></li>
         <li><div class="pier-bubble-menu--list-link">Alert 2</div></li>
         <li><div class="pier-bubble-menu--list-link">Alert 3</div></li>
+        <li class="pier-bubble-menu--list-divider"></li>
+        <li><div class="pier-bubble-menu--list-link">Clear Alerts</div></li>
       </ul>
       </div>
     </Fragment>
--- src/documentation/components/button-standard/block.js
+++ src/documentation/components/button-standard/block.js
@@ -3,8 +3,5 @@ import { Fragment } from 'react';
 export default (
     <Fragment>
         <button className="pier-button-standard pier-button-standard--block -m-b-4">Button Block</button>
-        <button className="pier-button-standard pier-button-standard--block--sm -m-b-4">Button Block @ SM</button>
-        <button className="pier-button-standard pier-button-standard--block--md -m-b-4">Button Block @ MD</button>
-        <button className="pier-button-standard pier-button-standard--block--lg">Button Block @ LG</button>
     </Fragment>
 );
