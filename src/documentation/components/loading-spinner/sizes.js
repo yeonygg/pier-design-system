2962108af0475ed8be0d14bd2aeaba84cd5b09fc
yeonygg <vivienne@gumgum.com>
2021-05-20 15:12:20 -0400

    added loading spinner options

--- src/documentation/components/loading-spinner/sizes.js
+++ src/documentation/components/loading-spinner/sizes.js
@@ -2,6 +2,15 @@ import { Fragment } from 'react';
 
 export default (
     <Fragment>
-    <div className="pier-loading-spinner2"></div>
+    <div className="-d-flex">
+    <div className="pier-loading-spinner2 -m-r-5"></div>
+    <div className="pier-loading-spinner3 -m-r-5"></div>
+    <div className="pier-loader">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+</div>
+    </div>
     </Fragment>
 );
