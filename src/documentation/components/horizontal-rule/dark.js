348a7bf854b9f4e4ba20cfe403d3fdb9f7ae01e8
yeonygg <vivienne@gumgum.com>
2021-06-08 13:07:54 -0400

    fixed button active scaling

--- src/documentation/components/horizontal-rule/dark.js
+++ src/documentation/components/horizontal-rule/dark.js
@@ -4,8 +4,8 @@ export default
 <Fragment>
 <div className="pier-container">
 <hr className="pier-hr -m-b-5"></hr>
-<hr className="pier-hr pier-hr--primary -m-b-5"></hr>
+<hr className="pier-hr pier-hr--white -m-b-5"></hr>
 <hr className="pier-hr pier-hr--dark -m-b-5"></hr>
-<hr className="pier-hr pier-hr--hero-thin"></hr>
+<hr className="pier-hr pier-hr--hero"></hr>
 </div>
 </Fragment>
