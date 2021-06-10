f255bee32e05b97199c1d8dc51073702c83fbd00
yeonygg <vivienne@gumgum.com>
2021-06-10 19:01:05 -0400

    fixed card and well bugs

--- src/documentation/components/card/color.js
+++ src/documentation/components/card/color.js
@@ -4,8 +4,8 @@ export default (
     <Fragment>
     <div className="pier-card pier-card--primary">
     <div className="pier-section">
-    <h1 className="pier-heading pier-heading--sm pier-heading--primary">Card Header</h1>
-    <p className="pier-body-text pier-body-text--primary">
+    <h1 className="pier-heading pier-heading--sm -c-cyan-dark-3">Card Header</h1>
+    <p className="pier-body-text -c-cyan-dark-3">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 </p>
@@ -24,8 +24,8 @@ export default (
 
     <div className="pier-card pier-card--green">
     <div className="pier-section">
-    <h1 className="pier-heading pier-heading--sm pier-heading--green">Card Header</h1>
-    <p className="pier-body-text pier-body-text--green">
+    <h1 className="pier-heading pier-heading--sm -c-green-dark-3">Card Header</h1>
+    <p className="pier-body-text -c-green-dark-3">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 </p>
@@ -34,8 +34,8 @@ export default (
 
     <div className="pier-card pier-card--blue">
     <div className="pier-section">
-    <h1 className="pier-heading pier-heading--sm pier-heading--blue">Card Header</h1>
-    <p className="pier-body-text pier-body-text--blue">
+    <h1 className="pier-heading pier-heading--sm -c-blue-dark-3">Card Header</h1>
+    <p className="pier-body-text -c-blue-dark-3">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 </p>
--- src/documentation/components/card/dark.js
+++ src/documentation/components/card/dark.js
@@ -15,8 +15,8 @@ export default (
 
     <div className="pier-card pier-card--dark pier-card--primary">
     <div className="pier-section">
-    <h1 className="pier-heading pier-heading--sm pier-heading--dark pier-heading--primary">Card Header</h1>
-    <p className="pier-body-text pier-body-text--dark pier-body-text--primary">
+    <h1 className="pier-heading pier-heading--sm pier-heading--dark -c-cyan-light-2">Card Header</h1>
+    <p className="pier-body-text pier-body-text--dark -c-cyan-light-2">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 </p>
@@ -35,8 +35,8 @@ export default (
 
     <div className="pier-card pier-card--dark pier-card--green">
     <div className="pier-section">
-    <h1 className="pier-heading pier-heading--sm pier-heading--dark pier-heading--green">Card Header</h1>
-    <p className="pier-body-text pier-body-text--dark pier-body-text--green">
+    <h1 className="pier-heading pier-heading--sm pier-heading--dark -c-green-light-2">Card Header</h1>
+    <p className="pier-body-text pier-body-text--dark -c-green-light-2">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 </p>
@@ -45,8 +45,8 @@ export default (
 
     <div className="pier-card pier-card--dark pier-card--blue">
     <div className="pier-section">
-    <h1 className="pier-heading pier-heading--sm pier-heading--dark pier-heading--blue">Card Header</h1>
-    <p className="pier-body-text pier-body-text--dark pier-body-text--blue">
+    <h1 className="pier-heading pier-heading--sm pier-heading--dark -c-blue-light-2">Card Header</h1>
+    <p className="pier-body-text pier-body-text--dark -c-blue-light-2">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 </p>
