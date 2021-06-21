4ad5fd42c0730b64a158bcf46d74c3d2db2b97cd
yeonygg <vivienne@gumgum.com>
2021-06-21 14:22:16 -0400

    gumgum loading spinner sizes added

---
+++ src/documentation/components/loading-spinner/gumgum-sizes.js
@@ -0,0 +1,49 @@
+import { Fragment } from 'react';
+
+export default (
+    <Fragment>
+
+<svg className="gg-loading-spinner--sm" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMaxYMid meet" viewBox="0 0 110 110" height="50" width="50">
+    <defs>
+        <linearGradient id="linear-gradient" x1="24.92" y1="77.18" x2="134.22" y2="77.18" gradientUnits="userSpaceOnUse">
+            <stop offset="0" stopColor="#00ce7c"></stop>
+            <stop offset="1" stopColor="#4ac1e0"></stop>
+        </linearGradient>
+    </defs>
+    <g id="Layer_2" dataname="Layer 2">
+        <path
+            class="path"
+            stroke="url(#linear-gradient)"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="16px"
+            d="M43.92,123.29a11,11,0,0,1-11-11v-9.78a11,11,0,0,1,11-11h71.3a11,11,0,0,0,11-11V42.07a11,11,0,0,0-11-11H76.61a11,11,0,0,0-11,11v70.22a11,11,0,0,1-11,11Z"
+            transform="translate(-24.92 -23.07)"
+        ></path>
+    </g>
+</svg>
+
+<svg className="gg-loading-spinner--md -m-l-4" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMaxYMid meet" viewBox="0 0 110 110" height="50" width="50">
+    <defs>
+        <linearGradient id="linear-gradient" x1="24.92" y1="77.18" x2="134.22" y2="77.18" gradientUnits="userSpaceOnUse">
+            <stop offset="0" stopColor="#00ce7c"></stop>
+            <stop offset="1" stopColor="#4ac1e0"></stop>
+        </linearGradient>
+    </defs>
+    <g id="Layer_2" dataname="Layer 2">
+        <path
+            class="path"
+            stroke="url(#linear-gradient)"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="16px"
+            d="M43.92,123.29a11,11,0,0,1-11-11v-9.78a11,11,0,0,1,11-11h71.3a11,11,0,0,0,11-11V42.07a11,11,0,0,0-11-11H76.61a11,11,0,0,0-11,11v70.22a11,11,0,0,1-11,11Z"
+            transform="translate(-24.92 -23.07)"
+        ></path>
+    </g>
+</svg>
+
+    </Fragment>
+);
