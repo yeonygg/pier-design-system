e267a9a19fba8a052ac3348106a81bbd57885034
yeonygg <vivienne@gumgum.com>
2021-06-21 13:18:10 -0400

    fixed loading spinners sizes and added easing

--- src/documentation/components/loading-spinner/dark.js
+++ src/documentation/components/loading-spinner/dark.js
@@ -2,28 +2,23 @@ import { Fragment } from 'react';
 
 export default (
     <Fragment>
-    <svg className="pier-loading-spinner" width="50" height="50">
+    <svg className="pier-loading-spinner" preserveAspectRatio="xMaxYMid meet" viewBox="0 0 100 100" height="50px" width="50px">
     <defs>
         <linearGradient id="gradient1" x2="1" y2="0">
-            <stop offset="20%" stopColor="#25B9EF" />
-            <stop offset="100%" stopColor="#08D18B" />
+            <stop offset="20%" stopColor="#25B9EF"></stop>
+            <stop offset="100%" stopColor="#08D18B"></stop>
         </linearGradient>
         <linearGradient id="gradient2" x2="1" y2="0">
-            <stop offset="20%" stopColor="#25B9EF" />
-            <stop offset="100%" stopColor="rgba(0,255,255,0)" />
+            <stop offset="20%" stopColor="#25B9EF"></stop>
+            <stop offset="100%" stopColor="rgba(0,255,255,0)"></stop>
         </linearGradient>
-<mask id="clip-rectangle">
-        <circle id="outer" cx="25" cy="25" r="25" fill="#ffffff"></circle> 
-        <circle id="inner" cx="25" cy="25" r="21"></circle> 
-    </mask>
-
+        <mask id="clip-rectangle">
+            <circle cx="50" cy="50" r="50" fill="#fff"></circle>
+            <circle cx="50" cy="50" r="43"></circle>
+        </mask>
     </defs>
-
-    <g>
-    <rect width="50" height="25" fill="url(#gradient1)" style={{ mask: 'url(#clip-rectangle)' }}></rect>
-    <rect width="50" height="25" y="25" fill="url(#gradient2)" style={{ mask: 'url(#clip-rectangle)' }}></rect>
-    </g>
-
-</svg>
+    <path fill="url(#gradient1)" d="M0 0H100V50H0z" mask="url(#clip-rectangle)"></path>
+    <path fill="url(#gradient2)" d="M0 50H100V100H0z" mask="url(#clip-rectangle)"></path>
+    </svg>
     </Fragment>
 );
--- src/documentation/components/loading-spinner/sizes.js
+++ src/documentation/components/loading-spinner/sizes.js
@@ -25,5 +25,48 @@ export default (
         </g>
     </svg>
 
+    <svg className="pier-loading-spinner--md -m-l-4" viewBox="0 0 100 100" preserveAspectRatio="xMaxYMid meet">
+    <defs>
+        <linearGradient id="gradient1" x2="1" y2="0">
+            <stop offset="20%" stopColor="#25B9EF" />
+            <stop offset="100%" stopColor="#08D18B" />
+        </linearGradient>
+        <linearGradient id="gradient2" x2="1" y2="0">
+            <stop offset="20%" stopColor="#25B9EF" />
+            <stop offset="100%" stopColor="rgba(0,255,255,0)" />
+        </linearGradient>
+        <mask id="clip-rectangle">
+            <circle id="outer" cx="50" cy="50" r="50" fill="#ffffff"></circle>
+            <circle id="inner" cx="50" cy="50" r="45"></circle>
+        </mask>
+    </defs>
+
+    <g>
+        <rect width="100" height="50" fill="url(#gradient1)" mask="url(#clip-rectangle)"></rect>
+        <rect width="100" height="50" y="50" fill="url(#gradient2)" mask="url(#clip-rectangle)"></rect>
+    </g>
+</svg>
+
+<svg className="pier-loading-spinner--lg -m-l-4" viewBox="0 0 100 100" preserveAspectRatio="xMaxYMid meet">
+<defs>
+    <linearGradient id="gradient1" x2="1" y2="0">
+        <stop offset="20%" stopColor="#25B9EF" />
+        <stop offset="100%" stopColor="#08D18B" />
+    </linearGradient>
+    <linearGradient id="gradient2" x2="1" y2="0">
+        <stop offset="20%" stopColor="#25B9EF" />
+        <stop offset="100%" stopColor="rgba(0,255,255,0)" />
+    </linearGradient>
+    <mask id="clip-rectangle">
+        <circle id="outer" cx="50" cy="50" r="50" fill="#ffffff"></circle>
+        <circle id="inner" cx="50" cy="50" r="45"></circle>
+    </mask>
+</defs>
+
+<g>
+    <rect width="100" height="50" fill="url(#gradient1)" mask="url(#clip-rectangle)"></rect>
+    <rect width="100" height="50" y="50" fill="url(#gradient2)" mask="url(#clip-rectangle)"></rect>
+</g>
+</svg>
     </Fragment>
 );
