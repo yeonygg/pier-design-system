4dc49302387210a995b8dba2132063137a1d588d
yeonygg <vivienne@gumgum.com>
2021-07-16 13:03:41 -0400

    added utilities homepage

--- src/pages/utilities/index.js
+++ src/pages/utilities/index.js
@@ -1,11 +1,26 @@
 import Head from 'next/head';
 import PropTypes from 'prop-types';
-import API from 'src/documentation/API';
 import { Fragment } from 'react';
+import API from 'src/documentation/API';
+import NextLink from 'next/link';
 import Layout from 'src/pages-components/Layout';
+import PageHeading from 'src/pages-components/PageHeading';
+import Section from 'src/pier-design-system/components/containers/Section';
+import Card from 'src/pier-design-system/components/containers/Card';
+import Heading from 'src/pier-design-system/components/text/Heading';
+import BodyText from 'src/pier-design-system/components/text/BodyText';
+import Link from 'src/pier-design-system/components/text/Link';
 import BodyContent from 'src/pages-components/BodyContent';
+import utilities from '../api/utilities';
 
-export default function Home({ appData }) {
+export default function UtilitiesIndex({ appData }) {
+    const utilities = appData.utilities;
+    //components is an array of objects
+    const breadcrumbs = [
+        { label: 'Home', link: '/', disabled: false },
+        { label: 'Utilities', link: '', disabled: true },
+    ];
+
     return (
         <Fragment>
             <Head>
@@ -13,13 +28,41 @@ export default function Home({ appData }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Layout appData={appData}>
-                <BodyContent></BodyContent>
+                <BodyContent>
+                    <PageHeading
+                        breadcrumbs={breadcrumbs}
+                        title="Utilities"
+                        lastUpdated="7/16/2021"
+                        description="This is a list of utilities for Pier Design System."
+                    ></PageHeading>
+                    <Section>
+                        {utilities.map((item, index) => {
+                            return (
+                                <NextLink href={`/utilities/${item.slug}`}>
+                                    <a>
+                                        <Card isLink>
+                                            <Section>
+                                                <Heading size="sm" className="-m-b-2">
+                                                    <Link>{item.title}</Link>
+                                                </Heading>
+                                                <BodyText size="xs" color="light-gray">
+                                                    {item['last-updated']}
+                                                </BodyText>
+                                                <BodyText className="-m-b-0">{item.description}</BodyText>
+                                            </Section>
+                                        </Card>
+                                    </a>
+                                </NextLink>
+                            );
+                        })}
+                    </Section>
+                </BodyContent>
             </Layout>
         </Fragment>
     );
 }
 
-Home.propTypes = {
+UtilitiesIndex.propTypes = {
     appData: PropTypes.object,
 };
 
